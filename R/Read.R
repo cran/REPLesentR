62424925771d90b7fbde686b3b60bde2514fa40e
Sebastian Warnholz <wahani@gmail.com>
2019-11-25 19:00:02 +0000

    version 0.4.0

--- R/Read.R
+++ R/Read.R
@@ -1,17 +1,24 @@
-# Read Module
-#
-# Read creates a new module with one method: 'auto'. This method selects an
-# appropriate procedure for reading in the content of the file depending on the
-# file extension. This function should not be necessary to call interactively,
-# please go to the docs for \link{newPresentation}.
-#
-# @seealso \link{newPresentation}
-#
-# @export
+#' Read Module
+#'
+#' Read creates a new module with one method: 'auto'. This method selects an
+#' appropriate procedure for reading in the content of the file depending on the
+#' file extension. It should not be necessary to call this function
+#' interactively, please go to the docs for \link{newPresentation}.
+#'
+#' @seealso \link{newPresentation}
+#'
+#' @export
 Read <- function() {
   modules::module({
 
     modules::export("auto")
+
+    auto <- function(fileName, ...) {
+      fileName <- normalizePath(fileName, mustWork = TRUE)
+      fileExt <- tolower(tools::file_ext(fileName))
+      stopifnot(fileExt %in% c("plain", "md", "rmd"))
+      get(fileExt, mode = "function")(fileName, ...)
+    }
 
     plain <- function(fileName, ...) {
       stopifnot(file.exists(fileName))
@@ -19,6 +26,16 @@ Read <- function() {
     }
 
     md <- function(fileName, ...) {
+      # By request from Brian Ripley (2019-11-25) and 'Writing R Extensions' we
+      # have to use pandoc conditionally. This already happens in knitr::pandoc
+      # which is why I have never included a test like this into this function.
+      # For a lack of better ideas, I copied the exact two lines from
+      # knitr::pandoc and included them here to comply to this requirement.
+      # -- begin knitr::pandoc
+      exec <- tryCatch(rmarkdown::pandoc_exec(), error = function(e) Sys.which("pandoc"))
+      if (length(exec) != 1 || exec == "")
+        stop("Please install Pandoc (https://pandoc.org)")
+      # -- end knitr::pandoc
       stopifnot(file.exists(fileName))
       plainFile <- knitr::pandoc(fileName, format = "plain")
       plain(plainFile)
@@ -30,13 +47,6 @@ Read <- function() {
       mdFile <- sub(regex, "md", fileName)
       knitr::knit(fileName, mdFile, ...)
       md(mdFile)
-    }
-
-    auto <- function(fileName, ...) {
-      stopifnot(file.exists(fileName))
-      fileExt <- tolower(tools::file_ext(fileName))
-      stopifnot(fileExt %in% c("plain", "md", "rmd"))
-      get(fileExt, mode = "function")(fileName, ...)
     }
 
   })
--- inst/Introduction.md
+++ inst/Introduction.md
@@ -3,7 +3,7 @@
 # REPLesentR: Presentations inside the R REPL
 
 author: Sebastian Warnholz  
-date: 2018-10-30  
+date: 2019-11-25  
 email: wahani@gmail.com  
 
 
